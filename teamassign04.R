635bd303cf22eeb3fcbf219b5fd10dacff30f648
Gregory A. Wert <gaw8pa@virginia.edu>
2017-09-26 16:50:39 -0400

    Add files via upload

---
+++ teamassign04.R
@@ -0,0 +1,61 @@
+
+###########################
+#                         #
+#   Team Assignment 4     #
+#                         #
+###########################
+
+## Please submit one set of answers per team.                  ##
+## Your answers may be submitted as an annotated R file.       ##
+## Please submit your plots in a PDF as a separate attachment. ##
+#################################################################
+
+
+#################
+## Question 1: ##
+#################
+
+# For each part of this problem, you are to create (or find) a data set with about 30 observations 
+# that is suitable for simple linear regression and satisfies the given specifications. If it is 
+# not possible, explain why it is not possible. For each part, include a plot that shows the 
+# interesting points.
+#
+#   (a) The data set has a point that is clearly visible for all four types of residuals 
+#       discussed -- standardized, studentized, PRESS, R-student.
+#   (b) The data set has a point that stands out when viewing studentized residuals but not 
+#       when viewing standardized residuals.
+#   (c) The data set has a point that stands out when viewing PRESS residuals but not when 
+#       viewing standardized residuals.
+#   (d) The data set has a point that stands out when viewing R-student residuals but not when 
+#       viewing standardized residuals.
+#   (e) The data set has a point that stands out when viewing PRESS residuals but not when 
+#       viewing studentized residuals.
+#   (f) The data set has a point that stands out when viewing R-student residuals but not when 
+#       viewing PRESS residuals.
+
+
+
+#################
+## Question 2: ##
+#################
+
+# For this problem you will use the file "data-table-B2.XLS".
+#
+#   (a) Fit the model using all explanatory variables. Iteratively remove insignificant variables
+#       one-by-one until the all remaining variables are significant. Which variables remain in your model?
+#   (b) Compute each of the five types of residuals discussed in the textbook:
+#       Residuals; Standardized residuals; Studentized residuals; PRESS residuals; R-student residuals.
+#       You may use R functions.
+#   (c) Use the results from part(a) to decide if there appear to be any outliers and/or high 
+#       influence points.
+#   (d) Produce a normal probability plot of the R-student residuals and evaluate the plot for 
+#       signs of departures from normality.
+#   (e) Produce plots of the R-student residuals vs. 
+#       (1) the predicted values, and
+#       (2) each explanatory variable.
+#       What assumptions may not be satisfied? Explain.
+
+
+
+
+
